e2e203ec41d8377b512b87b2d5b17d5a5bdc004e
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-16 11:11:40 +0300

    added separate  form

--- edit.php
+++ edit.php
@@ -1,5 +1,17 @@
 <?php
 	//edit.php
 	
-
+	//id mida muudame
+	echo $_GET["edit"];
+	
+	// saada kätte kõige uuemad andmed selle id kohta
+	//numbrimärk ja värv
 ?>
+<h2>Muuda autot</h2>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+  	<label for="number_plate" >auto nr</label><br>
+	<input id="number_plate" name="number_plate" type="text" value=""><br><br>
+  	<label for="color" >värv</label><br>
+	<input id="color" name="color" type="text" value=""><br><br>
+  	<input type="submit" name="update" value="Salvesta">
+  </form>
