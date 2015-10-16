35981281f6151dae4993cd8f89ef81ac318ddf10
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-16 11:53:40 +0300

    edit function get single row

--- edit.php
+++ edit.php
@@ -1,17 +1,35 @@
 <?php
 	//edit.php
+	require_once("edit_functions.php");
 	
 	//id mida muudame
-	echo $_GET["edit"];
+	if(!isset($_GET["edit"])){
+		
+		// ei ole aadressieal ?edit=midagi
+		// suunan table.php lehele
+		header("location: table.php");
+		
+	}else{
+		// saada kätte kõige uuemad andmed selle id kohta
+		//numbrimärk ja värv
+		//küsime andmebaasist andmed id järgi
+		
+		//saadan kaasa id
+		$car_object = getSingleCarData($_GET["edit"]);
+		var_dump($car_object);
+	}
 	
-	// saada kätte kõige uuemad andmed selle id kohta
-	//numbrimärk ja värv
+	
+	
+	
+	
 ?>
 <h2>Muuda autot</h2>
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
   	<label for="number_plate" >auto nr</label><br>
-	<input id="number_plate" name="number_plate" type="text" value=""><br><br>
+	<input id="number_plate" name="number_plate" type="text" value="<?php echo $car_object->number_plate;?>" ><br><br>
   	<label for="color" >värv</label><br>
-	<input id="color" name="color" type="text" value=""><br><br>
-  	<input type="submit" name="update" value="Salvesta">
+	<input id="color" name="color" type="text" value="<?=$car_object->color;?>"><br><br>
+  	
+	<input type="submit" name="update" value="Salvesta">
   </form>
--- edit_functions.php
+++ edit_functions.php
@@ -1,4 +1,44 @@
 <?php
-	// edit_functions.php
 
+	require_once("../configglobal.php");
+	$database = "if15_romil_2";
+	
+	function getSingleCarData($edit_id){
+		
+		//echo "id on ".$edit_id;
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT number_plate, color FROM car_plates WHERE id=? AND deleted IS NULL");
+		//asendan ? märgi
+		$stmt->bind_param("i", $edit_id);
+		$stmt->bind_result($number_plate, $color);
+		$stmt->execute();
+		
+		//tekitan objekti
+		$car = new Stdclass();
+		
+		//saime ühe rea andmeid
+		if($stmt->fetch()){
+			// saan siin alles kasutada bind_result muutujaid
+			$car->number_plate = $number_plate;
+			$car->color = $color;
+			
+			
+		}else{
+			// ei saanud rida andmeid kätte
+			// sellist id'd ei ole olemas
+			// see rida võib olla kustutatud
+			header("Location: table.php");
+		}
+		
+		return $car;
+		
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}	
+	
+	
 ?>
